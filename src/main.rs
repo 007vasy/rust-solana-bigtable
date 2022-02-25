34b62c57a3f58279475f538d0fc3714d6ebf1ce5
ben <007vasy@gmail.com>
2022-02-25 15:53:01 +1100

    added byte decode

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use {
+    enum_iterator::IntoEnumIterator,
+    std::io::{self, BufReader, Read, Write},
+};
+
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Debug, Serialize, Deserialize, IntoEnumIterator)]
+pub enum CompressionMethod {
+    NoCompression,
+    Bzip2,
+    Gzip,
+    Zstd,
+}
+
+fn main() {
+    println!("Hello, world!");
+    let method_size = bincode::serialized_size(&CompressionMethod::NoCompression).unwrap();
+    println!("{}", method_size)
+}
