36f2a6f374fa5f504f41cb5c8ed7284df5010e31
ben <007vasy@gmail.com>
2022-03-03 11:48:31 +1100

    add example block output

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use {
     enum_iterator::IntoEnumIterator,
     std::io::{self, BufReader, Read, Write},
     serde::{Serialize, Deserialize},
+    serde::ser::{SerializeStruct,Serializer},
     solana_storage_bigtable::{LedgerStorage},
     futures::executor::block_on,
     std::fs::{self,File},
@@ -14,7 +15,36 @@ use {
     tokio::runtime
 };
 
+// #[derive(Clone, Debug, PartialEq)]
+// pub struct ConfirmedBlockWithOptionalMetadata {
+//     pub previous_blockhash: String,
+//     pub blockhash: String,
+//     pub parent_slot: Slot,
+//     pub transactions: Vec<TransactionWithOptionalMetadata>,
+//     pub rewards: Rewards,
+//     pub block_time: Option<UnixTimestamp>,
+//     pub block_height: Option<u64>,
+// }
 
+// https://serde.rs/impl-serialize.html
+
+// impl Serialize for ConfirmedBlockWithOptionalMetadata {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//         S: Serializer,
+//     {
+//         // l7 is the number of fields in the struct.
+//         let mut state = serializer.serialize_struct("ConfirmedBlockWithOptionalMetadata", 7)?;
+//         state.serialize_field("previous_blockhash", &self.previous_blockhash)?;
+//         state.serialize_field("blockhash", &self.blockhash)?;
+//         state.serialize_field("parent_slot", &self.parent_slot)?;
+//         state.serialize_field("transactions", &self.transactions)?;
+//         state.serialize_field("rewards", &self.rewards)?;
+//         state.serialize_field("block_time", &self.block_time)?;
+//         state.serialize_field("block_height", &self.block_height)?;
+//         state.end()
+//     }
+// }
 
 fn get_file_as_byte_vec(filename: &String) -> Vec<u8> {
     let mut f = File::open(&filename).expect("no file found");
@@ -33,11 +63,15 @@ pub enum CompressionMethod {
     Zstd,
 }
 
+
 async fn do_fetch() {
     let sixty_sec = Duration::new(60, 0);
     let connection = LedgerStorage::new(true, std::option::Option::Some(sixty_sec), std::option::Option::Some("./solana-sandbox-86de2dfd579b.json".to_owned())).await.unwrap();
     let slot = 100010499;
-    println!("{:?}",connection.get_confirmed_block(slot).await);
+    let block_data = connection.get_confirmed_block(slot).await;
+    //let data_json_string = serde_json::to_string(&block_data.unwrap()).unwrap();
+    println!("{:?}", block_data);
+
     //get_confirmed_signatures_for_address
     // let address = b"57X5Rq3o7k5z976kAjYTWu5yKfgX1aQxH4bXACpmTPPF";
     // let pubkey = Pubkey::new(address);
